f11a938c98015d86be31da39e4b43bc823951283
kajal <kajal.gaharwal@gmail.com>
2022-11-16 14:59:31 +0530

    imgs

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -8,7 +8,9 @@ import Image3 from '../assets/3.jpeg';
 import Image4 from '../assets/4.jpeg';
 import Image5 from '../assets/5.jpeg';
 import Image6 from '../assets/6.jpeg';
-const Images = [Image1, Image2, Image3, Image4, Image5, Image6];
+import Image7 from '../assets/7.jpeg';
+import Image8 from '../assets/8.jpeg';
+const Images = [Image1, Image2, Image3, Image4, Image5, Image6, Image7, Image8];
 
 export default function FrontPage() {
   return (
@@ -16,8 +18,8 @@ export default function FrontPage() {
       <div className='imgCarousalBox'>
         <Carousel
           className='carousalBox'
-          height={300}
-          width={800}
+          height={400}
+          width={700}
           yOrigin={42}
           yRadius={48}
           autoPlay={true}>
@@ -37,19 +39,19 @@ export default function FrontPage() {
         <h3>Blessing</h3>
         <p>of our Son</p>
         <h2>♡Greetik♡</h2>
-        <p>(Son of Anchal and gajendra singh deval) </p>
+        <p>(Son of Anchal and Gajendra Singh Deval) </p>
         <h4 style={{ textDecoration: 'underline' }}>Functions</h4>
         <div className='functions'>
           <h3>Katha - 9 am</h3>
-          <h3>Jalwa poojan - 4 pm</h3>
+          <h3>Jalwa Poojan - 4 pm</h3>
           <h3>Dinner - 6 pm onwards</h3>
         </div>
         <h3 style={{ textDecoration: 'underline' }}>
-          Monday, november 21, 2022
+          Monday, November 21, 2022
         </h3>
         <p style={{ textDecoration: 'underline' }}>Venue</p>
-        <h3>mangalam c-181, kirti nagar,</h3>
-        <h3> magra poonjla, jodhpur</h3>
+        <h3>Mangalam C-181, Kirti Nagar,</h3>
+        <h3> Magra Poonjla, Jodhpur</h3>
       </div>
     </div>
   );
